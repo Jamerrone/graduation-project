837d94c0eb18ba7da94b3b15537c7aa4bca7bddb
James Peter Perrone Jefferies <james.p.perrone@protonmail.com>
2019-03-26 16:55:23 +0100

    feat: watch function v1 (#8)

--- src/commands/input.js
+++ src/commands/input.js
@@ -1,10 +1,7 @@
-process.stdout.write('Loading...');
 const path = require('path');
 
 const shared = require('./shared');
 const {printEr} = require('../libraries/utils');
-process.stdout.clearLine();
-process.stdout.cursorTo(0);
 
 module.exports = (args) => {
   const filePath = args.input;
