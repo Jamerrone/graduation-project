4c535c242c63f6d448fe7a5c9505f7802280d11f
James Peter Perrone Jefferies <james.p.perrone@protonmail.com>
2019-03-29 14:40:44 +0100

    feat: add support for custom browserscopes

--- src/commands/input.js
+++ src/commands/input.js
@@ -3,10 +3,10 @@ const path = require('path');
 const shared = require('./shared');
 const {printEr} = require('../libraries/utils');
 
-module.exports = (args) => {
+module.exports = ({browserslist}, args) => {
   const filePath = args.input || args._[0];
 
   path.extname(filePath) === '.css'
-    ? shared(filePath, args)
+    ? shared(filePath, browserslist, args)
     : printEr(`"${filePath}" is not a valid CSS file.`);
 };
---
+++ src/libraries/browserslist.js
@@ -0,0 +1,47 @@
+const browserslist = require('browserslist');
+
+const {printEr} = require('../libraries/utils');
+
+const getBrowserslist = (usersBrowserslist) => {
+  try {
+    return formatBrowserslist(browserslist(usersBrowserslist));
+  } catch (error) {
+    return printEr(`Unknown browser query "${usersBrowserslist.join(', ')}". `);
+  }
+};
+
+const formatBrowserslist = (browserslist) => {
+  const browsersDictionary = {
+    and_chr: 'chrome_android',
+    and_ff: 'firefox_android',
+    chrome: 'chrome',
+    edge: 'edge',
+    firefox: 'firefox',
+    ie: 'ie',
+    ios_saf: 'safari_ios',
+    op_mob: 'opera_android',
+    opera: 'opera',
+    safari: 'safari',
+    samsung: 'samsunginternet_android',
+  };
+
+  return browserslist.reduce((acc, browser) => {
+    const b = browser.split(' ');
+    const browserName = b[0];
+    let browserVersion = b[1];
+
+    browserVersion = browserVersion.includes('-')
+      ? browserVersion.split('-')[0]
+      : browserVersion;
+
+    if (browsersDictionary[browserName]) {
+      acc[browsersDictionary[browserName]] = Number(browserVersion);
+    }
+
+    return acc;
+  }, {});
+};
+
+module.exports = {
+  getBrowserslist,
+};
